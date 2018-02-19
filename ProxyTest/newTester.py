bbab9fc6ce2e3b363ad0f5846e7f0bf78bf3dce1
Jacob Hurst <jhurst@cs.unm.edu>
2018-02-18 20:30:46 -0700

    NewTester slowly scans every IP from given text list, TODO: thread newTester so it is faster

--- ProxyTest/newTester.py
+++ ProxyTest/newTester.py
@@ -7,7 +7,7 @@ class ChromeTest():
 
     def test(self):
         input_file = open("filtered.txt", "r")
-
+        i = 0
         for proxy in input_file:
             driverLocation = "C:/Users/Jacob/Documents/chromedriver.exe" #/Users/danielguzman/Documents/workspace/chromedriver"
             os.environ["webdriver.chrome.driver"] = driverLocation
@@ -15,10 +15,13 @@ class ChromeTest():
             options = webdriver.ChromeOptions()
             options.add_argument('--proxy-server=%s' % proxy)
             options.add_experimental_option("detach", True)
-            # options.set_headless(True)
+            options.set_headless(True)
 
             driver = webdriver.Chrome(driverLocation, chrome_options=options)
             driver.get("http://www.ip-adress.eu/")
+            if not(driver.page_source.__contains__("ERR")): print(i, ":", driver.current_url, " Success:", proxy)
+
+            i = i + 1
 
 c = ChromeTest()
 c.test()
