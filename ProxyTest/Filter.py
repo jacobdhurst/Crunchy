8cae0031bf31eb19007aa2b7c040336651bca11e
Jacob <jhurst@unm.edu>
2018-02-25 23:35:35 -0700

    bug fixes

--- ProxyTest/Filter.py
+++ ProxyTest/Filter.py
@@ -1,12 +1,21 @@
-def execute(input):
+def main():
+    input_file = open("ProxyList.txt", "r+")
+    output_file = open("FilteredProxyList.txt", "w+")
+
     temp = new = ""
-    for line in input:
+
+    for line in input_file:
         for char in line:
             if char == ":" or char == "." or char.isdigit(): temp += char
+
         length = len(temp)
+
         if length > 10:
-            temp = temp[:length - 4] + ":" + temp[length - 4:]
+            # temp = temp[:length - 4] + ":" + temp[length - 4:]
             temp += "\n"
             new += temp
             temp = ""
-        input.write(new)
+
+    output_file.write(new)
+    input_file.close()
+    output_file.close()
--- ProxyTest/ProxyTest.py
+++ ProxyTest/ProxyTest.py
@@ -1,49 +1,58 @@
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 from queue import Queue
-import urllib3
 import threading
 import time
+import os
 
-
-input_file = 'filtered.txt'
-threads = 10
-
-queue = Queue()
-output = []
-
-class ThreadUrl(threading.Thread):
+class threadTest(threading.Thread):
     """Threaded Url Grab"""
-    def __init__(self, queue):
+    def __init__(self, queue, output):
         threading.Thread.__init__(self)
         self.queue = queue
+        self.output = output
 
     def run(self):
         while True:
             #grabs host from queue
-            proxy_info = self.queue.get()
+            proxyToTest = self.queue.get()
             try:
-                proxy_handler = urllib3.ProxyHandler({'http':proxy_info})
-                opener = urllib3.build_opener(proxy_handler)
-                opener.addheaders = [('User-agent','Mozilla/5.0')]
-                urllib3.install_opener(opener)
-                req = urllib3.request("https://www.google.com")
-                sock=urllib3.urlopen(req, timeout=120)
-                rs = sock.read(1000)
-                if '<title>Google</title>' in rs:
-                    output.append(('Working:',proxy_info))
+                driverLocation = "C:/Users/Jacob/Documents/chromedriver.exe" #"/usr/local/share/chromedriver" #"C:/Users/Jacob/Documents/chromedriver.exe"  # /Users/danielguzman/Documents/workspace/chromedriver"
+                os.environ["webdriver.chrome.driver"] = driverLocation
+
+                options = webdriver.ChromeOptions()
+                options.set_headless(True)
+                options.add_argument('--proxy-server=%s' % proxyToTest)
+                options.add_experimental_option("detach", True)
+
+                driver = webdriver.Chrome(driverLocation, chrome_options=options)
+                driver.get("http://google.com")
+                print(driver.title)
+                print(driver.window_handles)
+                print(proxyToTest)
+                if (driver.title == "Google"):
+                    # print ("Success:", proxyToTest)
+                    self.output.append(("Success:", proxyToTest))
                 else:
-                    raise ("Error: Not Google.")
+                    raise ("Error.")
             except:
-                output.append(('Not Working:',proxy_info))
+                # print ("Failure:", proxyToTest)
+                self.output.append(("Failure:", proxyToTest))
             #signals to queue job is done
             self.queue.task_done()
 
-start = time.clock()
 def main():
+    output = []
+    input_file = 'FilteredProxyList.txt'
+    queue = Queue()
+
+    start = time.clock()
     # spawn a pool of threads, and pass them queue instance
-    for i in range(5):
-        t = ThreadUrl(queue)
+    for i in range(50): # 50 threads
+        t = threadTest(queue, output)
         t.setDaemon(True)
         t.start()
+
     hosts = [host.strip() for host in open(input_file).readlines()]
     # populate queue with data
     for host in hosts:
@@ -52,9 +61,8 @@ def main():
     # wait on the queue until everything has been processed
     queue.join()
 
+    for proxyToTest, host in output:
+      print(proxyToTest, host)
+    print("Elapsed Time: %lf seconds" % (time.clock() - start))
 
 main()
-for proxy, host in output:
-    print(proxy, host)
-
-print("Elapsed Time: %lf seconds" % (time.clock() - start))
